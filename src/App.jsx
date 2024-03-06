1c32898de4b300ae690a4019944291f27557812a
snidhesh <snidhesh@gmail.com>
2024-03-06 21:52:13 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// Assuming you also have an About component
+import About from './pages/About';
 
 export default function App() {
   return (
-    <h1 className="text-fuchsia-400">App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
