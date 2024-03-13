5f93de1289a50ec138045b15bdd0cdd4839bc2e0
snidhesh <snidhesh@gmail.com>
2024-03-13 18:24:27 +0530

    new updates

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 import  express  from "express";
-
 const app = express();
+import dotenv from 'dotenv';
+import mongoose  from "mongoose";
+import userRouter from './routes/user.route.js';
+
+//mongoose.connect(process.env.MONGO);
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  }) 
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3002, () => {
     console.log('Server running on port 3002');
 }
 
-
 );
+app.use('/api/user', userRouter);
