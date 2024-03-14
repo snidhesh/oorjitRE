d53ff22ecb0f537b5519a0114c376033c298449d
snidhesh <snidhesh@gmail.com>
2024-03-14 20:31:53 +0530

    full depoyment

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkYgypV1w0MCfF2j_B4b_MBHhwUE9mAms",
+  authDomain: "oorjitre.firebaseapp.com",
+  projectId: "oorjitre",
+  storageBucket: "oorjitre.appspot.com",
+  messagingSenderId: "387019799861",
+  appId: "1:387019799861:web:c8749e66dd4d567729207c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About oOrjit Real Estate</h1>
+      <p className='mb-4 text-slate-700'>oOrjit delivers customized digital workflows that connects every commerce interaction between suppliers, sellers and customers and optimizes every service aspect of your business end to end in one platform.</p>
+      <p className='mb-4 text-slate-700'>
+      Our e-commerce solutions help you manage all formats of commerce from one unified backend system, streamlining your commerce.
+
+</p>
+      <p className='mb-4 text-slate-700'>Transform your business digitally by digitalizing every commercial touchpoint and client engagement, from sales to service fulfillment, all within one platform.
+
+</p>
+    </div>
   )
 }
